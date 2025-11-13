82286ae3ebd9e7537e6f40db98de0517d3d5fc3f
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-13 03:45:25 +0300

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call() {
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh """
             docker build -t $DOCKER_USER/petclinic:latest .
             docker login -u $DOCKER_USER -p $DOCKER_PASS
