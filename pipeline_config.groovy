55154760d48417279b287e15e7a436cf03d9fadf
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-15 04:00:15 +0300

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,4 +1,9 @@
-library identifier: 'jenkins-pipelines@main', retriever: modernSCM(
-    [ $class: 'GitSCMSource', remote: 'https://github.com/RanaSalem2412/jenkins-pipelines.git' ]
+library(
+    identifier: 'jenkins-pipelines@main',
+    retriever: modernSCM([
+        $class: 'GitSCMSource',
+        remote: 'https://github.com/RanaSalem2412/jenkins-pipelines.git'
+    ])
 )
+
 pipelineTemplate = 'templates/petclinicPipeline.groovy'
