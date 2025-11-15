93f84b61a17a49f06b12e4276c5afb09c6e1c222
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-15 06:00:59 +0300

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,14 +1,3 @@
-import org.boozallen.plugins.jte.init.governance.config.dsl.PipelineConfigurationDsl
-
-PipelineConfigurationDsl.configure(this) {
-
-    // المكتبة المشتركة
-    library('jenkins-pipelines') {
-        default_version = 'main'
-    }
-
-    // Templates اللي الـ pipeline هيستخدمها
-    templates = [
-        'petclinicPipeline'
-    ]
+pipeline {
+    template('petclinicPipeline')
 }
