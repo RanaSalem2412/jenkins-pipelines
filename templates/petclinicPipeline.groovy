82099eeca6849c4592cc74b569089c0902662590
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-15 09:56:56 +0300

    Update petclinicPipeline.groovy

--- templates/petclinicPipeline.groovy
+++ templates/petclinicPipeline.groovy
@@ -1,44 +1,10 @@
-def call() {
-    pipeline {
-        agent { label 'docker-agent' }
-
-        stages {
-            stage('Checkout') {
-                steps {
-                    git branch: 'main', url: 'https://github.com/RanaSalem2412/spring-petclinic.git'
-                }
-            }
-
-            stage('Maven Build') {
-                steps {
-                    mavenBuild()
-                }
-            }
-
-            stage('SonarQube Scan') {
-                steps {
-                    sonarScan()
-                }
-            }
-
-            stage('Build Docker Image') {
-                steps {
-                    dockerBuild()
-                }
-            }
-
-            stage('Trivy Image Scan') {
-                steps {
-                    trivyScan()
-                }
-            }
-
-            stage('Push to Nexus') {
-                steps {
-                    nexusDeploy()
-                }
-            }
-        }
+pipeline {
+    stages {
+        mavenBuild()
+        sonarScan()
+        trivyScan()
+        nexusDeploy()
+        dockerBuild()
     }
 }
 
