7eba5ee64c59b57e99d601a4f31f78e4bef4cc90
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-15 03:31:14 +0300

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,27 @@
 def call() {
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+        // Build image locally
+        sh "docker build -t $DOCKER_USER/petclinic:latest ."
+
+        // Push to Docker Hub
         sh """
-            docker build -t $DOCKER_USER/petclinic:latest .
             docker login -u $DOCKER_USER -p $DOCKER_PASS
             docker push $DOCKER_USER/petclinic:latest
+        """
+    }
+
+    // Push to Nexus Docker registry
+    withCredentials([usernamePassword(credentialsId: 'nexus-admin', usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
+        def NEXUS_URL = "my-nexus-domain:8083"  // عدلي حسب عنوانك
+        def NEXUS_REPO = "repository/docker-repo"
+
+        // Tag image for Nexus
+        sh "docker tag $DOCKER_USER/petclinic:latest $NEXUS_URL/$NEXUS_REPO/petclinic:latest"
+
+        // Login & push to Nexus
+        sh """
+            docker login $NEXUS_URL -u $NEXUS_USER -p $NEXUS_PASS
+            docker push $NEXUS_URL/$NEXUS_REPO/petclinic:latest
         """
     }
 }
