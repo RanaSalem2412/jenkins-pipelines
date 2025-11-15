b2f7bc538e9e3af8e5a1f8d52cd887a61cfe7407
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-15 03:53:49 +0300

    Create pipeline_config.groovy

---
+++ pipeline_config.groovy
@@ -0,0 +1,4 @@
+library identifier: 'jenkins-pipelines@main', retriever: modernSCM(
+    [ $class: 'GitSCMSource', remote: 'https://github.com/RanaSalem2412/jenkins-pipelines.git' ]
+)
+pipelineTemplate = 'templates/petclinicPipeline.groovy'
