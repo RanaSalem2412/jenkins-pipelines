7887f60622bb098265d8199b1c78854f470b2569
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-15 10:50:45 +0300

    Update petclinicPipeline.groovy

--- vars/petclinicPipeline.groovy
+++ vars/petclinicPipeline.groovy
@@ -1,1 +1,19 @@
-
+def call() {
+    node {
+        stage('Build with Maven') {
+            mavenBuild()
+        }
+        stage('SonarQube Scan') {
+            sonarScan()
+        }
+        stage('Trivy Scan') {
+            trivyScan()
+        }
+        stage('Deploy to Nexus') {
+            nexusDeploy()
+        }
+        stage('Docker Build & Push') {
+            dockerBuild()
+        }
+    }
+}
