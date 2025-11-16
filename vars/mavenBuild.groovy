4241813ddba858bc8068f2913d8a80251181fde1
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-16 19:36:34 +0300

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,6 +1,12 @@
 def call() {
-    withMaven(maven: 'Maven_3.9.11', mavenSettingsConfig: '1d55bb71-5054-437b-85d4-bcd6dd462e7f') {
-        sh "mvn clean install"
+    // استخدمنا الـ Maven installation اللي مسميه Maven_3.9.11
+    // واستخدمنا الـ Global Maven settings عن طريق الـ file ID مباشرة
+    withMaven(
+        maven: 'Maven_3.9.11', 
+        mavenSettingsConfig: 'global-maven-settings'
+    ) {
+        sh "mvn clean deploy -DskipTests=false"
+
     }
 }
 
