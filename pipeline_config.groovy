6e96ab4e32f10573c83d9088a3fc5ed7b8bd08de
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-16 19:30:53 +0300

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -8,8 +8,12 @@ globals {
     sonar_project_key = "spring-petclinic"
     sonar_cred_id = "sonar-token"
 
-    nexus_repo = "maven-releases"
-    nexus_cred_id = "nexus-creds"
+    
+    nexus_repo = "maven-releases-project"
+    nexus_url = "http://44.203.150.173:8081"
+    nexus_cred_id = "nexus-admin-creds"
+
+
 
     docker_registry = "nexus.example.com:8083"
     docker_repo = "petclinic"
