c4c3278a9388bea91892ad4f312468a962c12bed
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-16 20:03:06 +0300

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,12 +1,8 @@
-def call() {
-    // استخدمنا الـ Maven installation اللي مسميه Maven_3.9.11
-    // واستخدمنا الـ Global Maven settings عن طريق الـ file ID مباشرة
+def call(globals) {
     withMaven(
         maven: 'Maven', 
         mavenSettingsConfig: 'global-maven-settings'
     ) {
         sh "mvn clean deploy -DskipTests=false -DaltDeploymentRepository=${globals.nexus_repo}::default::${globals.nexus_url}/repository/${globals.nexus_repo}"
-
     }
 }
-
