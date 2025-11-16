4baf8e1b33b58d65d9603a2ffd258c2fcb117e21
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-17 01:16:57 +0300

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,14 +1,23 @@
 def call(globals) {
-    withMaven(
-        maven: 'Maven', 
-        mavenSettingsConfig: 'global-maven-settings'
-    ) {
-        sh """
-            # امسح كل الـ Spring Boot artifacts
-            rm -rf ~/.m2/repository/org/springframework/boot/ || true
-            
-            # اعمل build مع تجاهل الـ tests تماماً (مش هيعمل compile ولا run)
-            mvn clean deploy -U -Dmaven.test.skip=true -DaltDeploymentRepository=${globals.nexus_repo}::default::${globals.nexus_url}/repository/${globals.nexus_repo}
-        """
+    // استخدم الـ credentials من Jenkins (ID = nexus-admin)
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'nexus-admin', 
+            usernameVariable: 'NEXUS_USERNAME',
+            passwordVariable: 'NEXUS_PASSWORD'
+        )
+    ]) {
+        withMaven(
+            maven: 'Maven', 
+            mavenSettingsConfig: 'global-maven-settings'
+        ) {
+            sh """
+                # امسح كل الـ Spring Boot artifacts
+                rm -rf ~/.m2/repository/org/springframework/boot/ || true
+                
+                # اعمل build مع تجاهل الـ tests تماماً
+                mvn clean deploy -U -Dmaven.test.skip=true -DaltDeploymentRepository=${globals.nexus_repo}::default::${globals.nexus_url}/repository/${globals.nexus_repo}
+            """
+        }
     }
 }
