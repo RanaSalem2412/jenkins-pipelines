a08557a38e3154c8b7c8b69aa209e898414087d5
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-17 14:30:57 +0300

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -2,17 +2,14 @@ def call(Map config = [:]) {
 
     withCredentials([string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')]) {
 
-        def mavenOpts = config.maven_opts ?: ""
-        def sonarKey = config.sonar_project_key ?: ""
-        def sonarName = config.sonar_project_name ?: ""
-        def sonarHost = config.sonar_host_url ?: ""
-
-        sh """
-            mvn ${mavenOpts} sonar:sonar \
-                -Dsonar.projectKey=${sonarKey} \
-                -Dsonar.projectName=${sonarName} \
-                -Dsonar.host.url=${sonarHost} \
-                -Dsonar.login=$SONAR_TOKEN
-        """
+        sh '''
+        mvn -B \
+            -DskipTests=false \
+            sonar:sonar \
+            -Dsonar.projectKey=spring-petclinic \
+            -Dsonar.projectName=spring-petclinic \
+            -Dsonar.host.url=http://http://44.203.150.173:9000/ \
+            -Dsonar.login=$SONAR_TOKEN
+        '''
     }
 }
