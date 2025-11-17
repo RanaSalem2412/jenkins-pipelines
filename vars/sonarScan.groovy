836496267bcd2e7e781da737ed2da4ed3221b768
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-17 14:19:36 +0300

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,5 +1,18 @@
-def call() {
+def call(Map config = [:]) {
+
     withCredentials([string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')]) {
-        sh "mvn sonar:sonar -Dsonar.login=$SONAR_TOKEN"
+
+        def mavenOpts = config.maven_opts ?: ""
+        def sonarKey = config.sonar_project_key ?: ""
+        def sonarName = config.sonar_project_name ?: ""
+        def sonarHost = config.sonar_host_url ?: ""
+
+        sh """
+            mvn ${mavenOpts} sonar:sonar \
+                -Dsonar.projectKey=${sonarKey} \
+                -Dsonar.projectName=${sonarName} \
+                -Dsonar.host.url=${sonarHost} \
+                -Dsonar.login=$SONAR_TOKEN
+        """
     }
 }
