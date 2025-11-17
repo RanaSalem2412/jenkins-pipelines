90931a2da744c8c4d13efa9890357c55342e3772
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-17 22:41:25 +0300

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,14 +3,18 @@ def call(Map config = [:]) {
     def imageTag = config?.imageTag ?: "${env.BUILD_NUMBER ?: 'latest'}"
     def dockerfile = config?.dockerfile ?: 'Dockerfile'
     def context = config?.context ?: '.'
-    
+
     stage('Build Docker Image') {
         echo "🔨 Building Docker image: ${imageName}:${imageTag}"
         sh """
             docker build -f ${dockerfile} -t ${imageName}:${imageTag} ${context}
         """
         echo "✅ Image built successfully: ${imageName}:${imageTag}"
+
+        // إضافة Tag باسم latest
+        echo "🏷️ Tagging Docker image as ${imageName}:latest"
+        sh "docker tag ${imageName}:${imageTag} ${imageName}:latest"
     }
-    
+
     return "${imageName}:${imageTag}"
 }
