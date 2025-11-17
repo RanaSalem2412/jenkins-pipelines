e2b7edb1eb6097fae3bac003709feec001450213
RanaSalem2412 <ranasalem923@gmail.com>
2025-11-17 13:45:18 +0300

    Update petclinicPipeline.groovy

--- vars/petclinicPipeline.groovy
+++ vars/petclinicPipeline.groovy
@@ -1,7 +1,6 @@
 def call(globals) {   // تمرير globals
     pipeline {
         agent { label 'docker-agent' }
-
         stages {
             stage('Build') {
                 steps {
@@ -9,21 +8,24 @@ def call(globals) {   // تمرير globals
                     mavenBuild(globals)  // ينفذ mvn clean install/deploy
                 }
             }
-
             stage('SonarQube Scan') {
                 steps {
                     echo "Running SonarQube scan..."
                     sonarScan(globals)  // ينفذ تحليل SonarQube
                 }
             }
-
             stage('Docker Build') {
                 steps {
                     echo "Building Docker image..."
                     dockerBuild(globals)  // ينشئ صورة Docker
                 }
             }
-
+            stage('Trivy Scan') {
+                steps {
+                    echo "Scanning Docker image for vulnerabilities..."
+                    trivyScan(globals)  // يفحص الصورة بحثاً عن ثغرات أمنية
+                }
+            }
             stage('Push Docker Image') {
                 steps {
                     echo "Pushing Docker image to Nexus..."
